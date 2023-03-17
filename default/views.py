5eb87a5bbfd425746c207491355274ee1443eef0
musicmank545 <kuhnsmanj@me.com>
2023-03-17 19:23:54 -0400

    env

--- default/views.py
+++ default/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import os
+import json
 
 # Create your views here.
 def hello_world(request):
 
-    space = os.environ.get('SPACE','World')
-    html = "<html><body>Hello {}!</body></html>".format(space)
+    space = os.environ.get('SPACE',"""\{\}""")
+    space = json.loads(space)
+    html = "<html><body>Hello {}!</body></html>".format(space['SPACE'])
     return HttpResponse(html)
