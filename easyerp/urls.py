834a1288f1054b365d0162276f95c8ea3978f17e
musicmank545 <55155882+musicmank545@users.noreply.github.com>
2023-03-22 13:26:15 -0400

    updates

--- easyerp/urls.py
+++ easyerp/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 from django.views.static import serve
 
 urlpatterns = [
-    path('', admin.site.urls),
-    path('test/',include('default.urls')),
+    path('admin/', admin.site.urls),
+    path('',include('default.urls')),
     re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
