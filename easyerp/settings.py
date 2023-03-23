12067454f775b86ccc903d72c09c38dead303cd6
musicmank545 <kuhnsmanj@me.com>
2023-03-22 23:57:38 -0400

    try again

--- easyerp/settings.py
+++ easyerp/settings.py
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
 
 
 MIDDLEWARE = [
-    'django_prometheus.middleware.PrometheusBeforeMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -54,7 +53,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django_prometheus.middleware.PrometheusAfterMiddleware',
 ]
 
 ROOT_URLCONF = 'easyerp.urls'
