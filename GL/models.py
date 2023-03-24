f153a2b6ddf63b4741393b258ead83d379870815
lrichardsawsdd <lrichards.awsdd@gmail.com>
2023-03-24 17:05:07 -0400

    add journal entry header and detail models

--- GL/models.py
+++ GL/models.py
@@ -9,3 +9,20 @@ class Account(models.Model):
 
     def __str__(self):
           return self.AccountName
+
+class JournalEntryHeader(models.Model):
+    id = models.UUIDField(
+         primary_key = True,
+         default = uuid.uuid4,
+         editable = False)
+    Description = models.CharField(max_length=250)
+
+    
+class JournalEntryDetail(models.Model):
+    id = models.UUIDField(
+         primary_key = True,
+         default = uuid.uuid4,
+         editable = False)
+    Account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    DebitOrCredit = models.CharField(max_length=250)
+    Amount =  models.FloatField(default=0)
